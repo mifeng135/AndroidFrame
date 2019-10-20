edaa7262cf850bf13ba547113cd395756e80603a
mifeng <zhoujian@163.com>
2019-10-20 12:08:23 +0800

    --add sample png

--- app/src/main/java/com/mifeng/mf/TabViewController/MineViewController.java
+++ app/src/main/java/com/mifeng/mf/TabViewController/MineViewController.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.mifeng.mf.Common.MFPostEvent;
@@ -16,6 +18,7 @@ import com.mifeng.mf.MFNavigation.Controller.UIViewController;
 import com.mifeng.mf.MFNavigation.Controller.TabViewController;
 import com.mifeng.mf.MFNavigation.Navigator.Navigator;
 import com.mifeng.mf.R;
+import com.mifeng.mf.UIViewController.LoginViewController;
 
 /**
  * Created by Administrator on 2019/8/26.
@@ -34,7 +37,19 @@ public class MineViewController extends UIViewController<LinearLayout> implement
         view = createLinearLayout();
         View baseView = inflater.inflate(R.layout.mine_layout,view,false);
         view.addView(baseView);
+        initVIiewTouch(baseView);
         return view;
+    }
+
+    private void initVIiewTouch(View baseView) {
+        TextView login = baseView.findViewById(R.id.mine_push_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginViewController loginViewController = new LoginViewController(getActivity(),"loginViewController",new HashMap());
+                Navigator.getInstance().push(getId(),loginViewController);
+            }
+        });
     }
 
     @Override
